06917a70866153b3d50050d2fcfcc8ff578895c6
ronyzrihen <ronyzn3@gmail.com>
2024-02-26 14:50:29 +0200

    added cores middlewere from presentation to tintagrate react

--- server/app.js
+++ server/app.js
@@ -4,6 +4,12 @@ const { errorHandler } = require('../middlewares/errorHendler');
 require('express-async-errors');
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.set('Content-Type', 'application/json');
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
