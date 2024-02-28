551e69509e9a94ee6713433266a0822bc8633ede
ronyzrihen <ronyzn3@gmail.com>
2024-02-28 18:45:57 +0200

    installed cors

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 const { errorHandler } = require('../middlewares/errorHendler');
 require('express-async-errors');
 
 const app = express();
+app.use(cors());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.set('Content-Type', 'application/json');
   next();
